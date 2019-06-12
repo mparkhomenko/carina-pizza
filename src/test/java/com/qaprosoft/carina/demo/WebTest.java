1ce3f2ce54e16afdbc53092cce242ada06083907
Maksim <maksim.parkhomenko96@gmail.com>
2019-06-12 12:29:45 +0300

    fix

---
+++ src/test/java/com/qaprosoft/carina/demo/WebTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2019 QAPROSOFT (http://qaprosoft.com/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qaprosoft.carina.demo;
+
+import java.util.List;
+
+import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestTag;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.qaprosoft.carina.core.foundation.AbstractTest;
+import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.carina.demo.gui.pages.HomePage;
+
+/**
+ * This sample shows how create Web test.
+ *
+ * @author qpsdemo
+ */
+public class WebTest extends AbstractTest {
+    @Test
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    @TestTag(name = "area test", value = "web")
+    public void testCompare() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+
+        String title = homePage.getDriver().getTitle();
+        Assert.assertTrue(title.equals("Пицца Лисицца"));
+    }
+}
